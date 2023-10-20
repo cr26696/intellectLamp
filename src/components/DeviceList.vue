0dc4276602f55cc97ec02f7cebab15c6c25c5e16
imbaXmz0121 <imbaXmz@gmail.com>
2023-10-20 18:40:41 +0800

    debug

--- src/components/DeviceList.vue
+++ src/components/DeviceList.vue
@@ -13,7 +13,7 @@
         opacity: 1;
         border-radius: 24.5px;
         background: rgba(255, 255, 255, 1);
-        box-shadow: 0px 8px 18px  rgba(0, 0, 0, 0.1">重置</button>
+        box-shadow: 0px 8px 18px  rgba(0, 0, 0, 0.1)">重置</button>
       <button style="float: right;
         width: 120px;
         height: 50px;
@@ -231,23 +231,9 @@ export default {
   name: 'DeviceList',
   data: function () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      tableData: [
+        
+      ]
     }
   }
 }
