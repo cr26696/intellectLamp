6fbbbf62b10d9423e83864983690bae0977c2e2a
cr26696 <2669648220@qq.com>
2023-10-22 10:44:23 +0800

    el容器样式简化

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,9 +1,9 @@
 <template>
 
 <el-container id="container">
-  <el-header>Header</el-header>
+  <el-header height="94px">Header</el-header>
   <el-container>
-    <el-aside><SideBar></SideBar></el-aside>
+    <el-aside width="233px"><SideBar></SideBar></el-aside>
     <el-main><router-view/></el-main>
   </el-container>
 </el-container>
@@ -25,21 +25,16 @@ export default {
  .el-header{
     background-color: hsla(183, 13%, 67%, 0.85);
     color: #333;
-    text-align: center;
-    height: 94px !important;
   }
 
   .el-aside {
     background-color: hsla(133, 30%, 58%, 0.85);
     color: #333;
-    text-align: center;
-    width: 233px !important;
   }
 
   .el-main {
     background-color: hsla(140, 100%, 98%, 0.85);
     color: #333;
-    text-align: center;
     position: relative;
     padding: 0 !important;
   }
