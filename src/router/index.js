47b17086ef148080ea2f38a3112ed3702e848deb
cr26696 <2669648220@qq.com>
2023-10-22 17:22:19 +0800

    路由中删除已经弃用的控制面板

--- src/router/index.js
+++ src/router/index.js
@@ -6,13 +6,6 @@ import DeviceList from '@/components/DeviceList.vue'
 import DeviceControl from '@/components/DeviceControl.vue'
 import AlarmInfo from '@/components/AlarmInfo.vue'
 
-import ParamSetting from '@/components/device_control_detail/ParamSetting.vue'
-import SwitchSetting from '@/components/device_control_detail/SwitchSetting.vue'
-import TimerSetting from '@/components/device_control_detail/TimerSetting.vue'
-import ModeSetting from '@/components/device_control_detail/ModeSetting.vue'
-import RebootSetting from '@/components/device_control_detail/RebootSetting.vue'
-import CarSetting from '@/components/device_control_detail/CarSetting.vue'
-
 Vue.use(VueRouter)
 
 const routes = [
@@ -35,37 +28,7 @@ const routes = [
       },
       {
         path: 'deviceControl',
-        component: DeviceControl,
-        children: [
-          {
-            path: '/',
-            redirect: 'paramsetting'
-          },
-          {
-            path: 'paramsetting',
-            component: ParamSetting
-          },
-          {
-            path: 'switchsetting',
-            component: SwitchSetting
-          },
-          {
-            path: 'timersetting',
-            component: TimerSetting
-          },
-          {
-            path: 'modesetting',
-            component: ModeSetting
-          },
-          {
-            path: 'rebootsetting',
-            component: RebootSetting
-          },
-          {
-            path: 'carsetting',
-            component: CarSetting
-          }
-        ]
+        component: DeviceControl
       },
       {
         path: 'alarmInfo',
